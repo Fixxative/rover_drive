941a6f9e6598fc3624b923aef37ba1c2ccdaee21
Fixxative <100667440+Fixxative@users.noreply.github.com>
2021-12-16 13:23:43 +0000

    fix merge problem

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -18,4 +18,5 @@ fn about<'a>() -> (Paragraph<'a>, u16) {
         Spans::from(Span::styled("   ##       ##   ##     ##    #######   ##         ##     ## ##    ####    ", Style::default().fg(Color::LightCyan))),
         Spans::from(Span::styled("   ##       ##   ##     ##    ## ####   ##         ##     ## ##    ##      ", Style::default().fg(Color::LightCyan))),
         Spans::from(Span::styled("    ##  ##  ### ###     ##    ##  ###   ##  ##     ##      ###     ##   #  ", Style::default().fg(Color::LightCyan))),
-        Spans::from(Span::styled("     ####    #####    ######  ##   ##  #######   ######    ###    #######  "
+        Spans::from(Span::styled("     ####    #####    ######  ##   ##  #######   ######    ###    #######  ", Style::default().fg(Color::LightCyan))),
+        Spans::from(V

f15747c5c0e10d5df12dec10c48b792f805b8f82
Fixxative <100667440+Fixxative@users.noreply.github.com>
2021-12-16 06:31:05 +0000

    xhci: check for failed dma pool allocation

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -18,4 +18,4 @@ fn about<'a>() -> (Paragraph<'a>, u16) {
         Spans::from(Span::styled("   ##       ##   ##     ##    #######   ##         ##     ## ##    ####    ", Style::default().fg(Color::LightCyan))),
         Spans::from(Span::styled("   ##       ##   ##     ##    ## ####   ##         ##     ## ##    ##      ", Style::default().fg(Color::LightCyan))),
         Spans::from(Span::styled("    ##  ##  ### ###     ##    ##  ###   ##  ##     ##      ###     ##   #  ", Style::default().fg(Color::LightCyan))),
-        Spans::from(Span::styled("     ####
+        Spans::from(Span::styled("     ####    #####    ######  ##   ##  #######   ######    ###    #######  "

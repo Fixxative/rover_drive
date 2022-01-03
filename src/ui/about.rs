161814806c52319eac5b2a393da640a708c46c96
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-01-03 12:38:21 +0000

    Cleaned up test

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -28,4 +28,5 @@ fn about<'a>() -> (Paragraph<'a>, u16) {
         Spans::from(Span::styled("(c) Mayer Analytics, GPL-3.0", Style::default().fg(Color::Red))),
         Spans::from(Vec::new()),
         Spans::from(Span::styled("https://github.com/mayeranalytics/coinlive", Style::default().fg(Color::Green))),
-        S
+        Spans::from(Vec::new()),
+        Spans::from(Span::styled(format!(

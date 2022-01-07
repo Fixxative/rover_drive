b6aa8bab3089fff3cfdb3d6e594a3ed8bde55388
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-01-07 11:04:21 +0000

    - updates

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -41,4 +41,5 @@ fn about<'a>() -> (Paragraph<'a>, u16) {
 /// Draw the message bar at the bottom
 pub fn draw_about<B: Backend>(f: &mut Frame<B>, area: Rect) {
     let about = about();
- 
+    let h = about.1; let about = about.0;
+    let vert_space = if area.he

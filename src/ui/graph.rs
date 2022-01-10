fe3fa911e943005fdd89d6290c4a15bc24e98659
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-01-10 14:58:53 +0000

    added missing images

--- src/ui/graph.rs
+++ src/ui/graph.rs
@@ -3,4 +3,5 @@ use crate::utils::*;
 use crate::ui::nice::{f64_nice_range, Nice};
 use tui::{
     style::{Style, Color, Modifier},
-    widgets::{Axis, Chart
+    widgets::{Axis, Chart, Widget, Block, Dataset, GraphType, Paragraph},
+    layout::{

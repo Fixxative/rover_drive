05703e7ba356c065a692b4a5e367c84f2fdc7d1c
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-01-11 17:43:09 +0000

    msm: rpc: Fix uninitialized union in rpc router close function

--- src/ui/graph.rs
+++ src/ui/graph.rs
@@ -4,4 +4,8 @@ use crate::ui::nice::{f64_nice_range, Nice};
 use tui::{
     style::{Style, Color, Modifier},
     widgets::{Axis, Chart, Widget, Block, Dataset, GraphType, Paragraph},
-    layout::{
+    layout::{Rect},
+    text::{Span},
+    buffer::{Buffer},
+    symbols
+}

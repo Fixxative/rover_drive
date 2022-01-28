b743b1cdd656b9a1a5bd9c5f866424240794324f
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-01-28 16:14:24 +0000

    Update quasarrat.txt

--- src/ui/graph.rs
+++ src/ui/graph.rs
@@ -46,4 +46,5 @@ impl<'a> Widget for Graph<'a> {
         let mut t_max: f64 = 0.0;
         let mut p_min: f64 = f64::MAX;
         let mut p_max: f64 = 0.0;
-        // we want to show high and low only. This gives
+        // we want to show high and low only. This gives a fuzzier, less crisp graph. 
+        // The advantage is, obviously, t

08fd838f69c0255f868d73763db2d34115df8e40
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-08-09 11:18:55 +0000

    Bump reflex-dom

--- src/ui/search.rs
+++ src/ui/search.rs
@@ -52,4 +52,6 @@ impl<'a> Widget for Search<'a> {
             }
             let style: tui::style::Style = if ix==cursor.0 && iy==cursor.1 {
                 *self.ref_i_symbol.borrow_mut() = i_symbol;
-                Style::default().fg(Color::Red).add_modifier(Modifier::BOLD).add
+                Style::default().fg(Color::Red).add_modifier(Modifier::BOLD).add_modifier(Modifier::ITALIC)
+            } else {
+                Style::de

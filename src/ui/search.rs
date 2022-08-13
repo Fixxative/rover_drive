01af5eb5ab859a9459d3fa51b7add445b0b5c720
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-08-13 11:25:59 +0000

    header added

--- src/ui/search.rs
+++ src/ui/search.rs
@@ -58,4 +58,6 @@ impl<'a> Widget for Search<'a> {
             };
             let span = Span::styled(format!("{}", info.symbol), style);
             let spans = Spans::from(vec![span]);
-            buf.set_spans(x, y as u16,
+            buf.set_spans(x, y as u16, &spans, symbol_width);
+            y += 1;
+            iy

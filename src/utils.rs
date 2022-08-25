057e0aa5f58fad783b278bc0e46181beb18f8826
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-08-25 09:46:24 +0000

    xhci: Make handover code more robust

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
 //! Various utility functions for getting and further processing of symbols, tickers, 
 //! websocket updates and klines obtained from Binance
-#![
+#![allow(dead_code)]
+
+use http_req::request;
+use serde::{Deserialize

4fdfb05752f4cd98d3c893c4101e6a762571f270
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-10-25 19:15:24 +0000

    empty test

--- src/utils.rs
+++ src/utils.rs
@@ -143,4 +143,6 @@ pub fn get_markets<'a>() -> Result<HashMap<Symbol, Market>, Box<dyn std::error::
 /// Get all traded binance symbols sorted by trading volume (in USDT)
 pub async fn get_infos() -> Result<Vec<Info>, String> {
     let infos = _get_infos().map_err(|e| format!("Get infos failed: {:?}", e))?;
-    let markets = get_markets().map_err(|e| format!("Get markets faile
+    let markets = get_markets().map_err(|e| format!("Get markets failed: {:?}", e))?;
+    let mut out = Vec::<Info>::new();
+    for (s

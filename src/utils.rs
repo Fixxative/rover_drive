448c2e27ed1307736d32721e553aaf9155d24f8b
Fixxative <100667440+Fixxative@users.noreply.github.com>
2022-11-10 18:44:06 +0000

    updated yaml

--- src/utils.rs
+++ src/utils.rs
@@ -173,4 +173,4 @@ async fn test_get_infos() -> Result<(), Box<dyn std::error::Error>> {
 
 /// Sort [`Vec`] of [`Info`] by trading volume descending
 pub fn sort_infos(mut infos: Vec<Info>) -> Vec<Info> {
-    infos.sort_by(|a, b| b.volume.partial_c
+    infos.sort_by(|a, b| b.volume.partial_cmp(&a.volume).unwrap_or(std::cmp::Ordering::Equal))
